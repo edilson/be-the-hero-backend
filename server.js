4ddb14d48f10b16753f558c3211ae2def92f6988
SEdilson <edilson.silva00@hotmail.com>
2020-03-28 00:37:34 -0300

    Separating middlewares from server

--- server.js
+++ server.js
@@ -1,15 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-const { errors } = require('celebrate')
-
-const routers = require('./src/routes');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(routers);
-app.use(errors());
+const app = require('./src/config/custom-express');
 
 app.listen(3333, () =>{
     console.log('Servidor escutando na porta 3333')
---
+++ src/config/custom-express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const { errors } = require('celebrate')
+
+const routers = require('../routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routers);
+app.use(errors());
+
+module.exports = app;
