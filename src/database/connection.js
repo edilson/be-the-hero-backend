632f7c74557e7fb961cd207598e730c3d17f4280
SEdilson <edilson.silva00@hotmail.com>
2020-03-30 23:08:33 -0300

    Refactor db connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,8 +1,6 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-// const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
-
 let config;
 
 if(process.env.NODE_ENV === 'test') {
